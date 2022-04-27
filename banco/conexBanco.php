c5b509f0d2f25b655c372b27fb5334e98c74e878
PCCaio <99839550+PCCaio@users.noreply.github.com>
2022-04-27 16:23:10 -0300

    Update conexBanco.php

--- banco/conexBanco.php
+++ banco/conexBanco.php
@@ -7,8 +7,26 @@ $user = "root";
 $pass = "";
 $bd = "crudphp";
 $driver = "mysql";
+try{
+    $conexao = new PDO("{$driver}:host={$host};dbname={$bd}",$user,$pass);
+}
+catch(PDOException $e){
+    switch ($e->getCode()) {
+        case 2002:
+            echo 'Erro no servidor, Host não encontrado';
+            break;
+        case 1049:
+            echo 'Erro no servidor, banco não encontrado';
+            break;
+        case 1044:
+            echo 'Erro no servidor, erro de usuário';
+            break;
+            default:
+                'Erro no servidor';
+            break;
+    }
+}
 
-$conexao = new PDO("{$driver}:host={$host};dbname={$bd}",$user,$pass);
 
 //tabela teste:
 $sql = $conexao->prepare("SELECT * FROM clientes");
