ff91f03565f30e3db4effcb9c20afe7036ef8d20
PCCaio <99839550+PCCaio@users.noreply.github.com>
2022-04-28 00:58:54 -0300

    Update conexBanco.php

--- banco/conexBanco.php
+++ banco/conexBanco.php
@@ -1,40 +1,49 @@
 <?php
 
-echo "<h2>conexao com banco de dados </h2>";
+class bd{
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$bd = "crudphp";
-$driver = "mysql";
-try{
-    $conexao = new PDO("{$driver}:host={$host};dbname={$bd}",$user,$pass);
-}
-catch(PDOException $e){
-    switch ($e->getCode()) {
-        case 2002:
-            echo 'Erro no servidor, Host não encontrado';
-            break;
-        case 1049:
-            echo 'Erro no servidor, banco não encontrado';
-            break;
-        case 1044:
-            echo 'Erro no servidor, erro de usuário';
-            break;
-            default:
-                'Erro no servidor';
-            break;
+    private $host="localhost",
+    $user ="root",
+    $pass="",
+    $bd="crudphp",
+    $driver="mysql";
+
+    private function conexao(){
+        try{
+            $conexao = new PDO("{$this->driver}:host={$this->host};dbname={$this->db}","{$this->user}","{$this->pass}");
+            return $conexao;
+        }
+        catch(PDOException $e){
+            switch ($e->getCode()) {
+                case 2002:
+                    echo 'Erro no servidor, Host não encontrado';
+                    break;
+                case 1049:
+                    echo 'Erro no servidor, banco não encontrado';
+                    break;
+                case 1044:
+                    echo 'Erro no servidor, erro de usuário';
+                    break;
+                    default:
+                        'Erro no servidor';
+                    break;
+            }
+            exit();
+        }
+    }   
+    public function executeSQL($sql){
+        $this->obj = $this->conexao()->prepare($sql);
+        
+    
+       return $this->obj->execute();
+
+    }
+
+    public function listData(){
+
+        return $this->obj->fetchALL();
     }
 }
-
-
-//tabela teste:
-$sql = $conexao->prepare("SELECT * FROM clientes");
-$sql->execute();
-
-foreach($sql->fetchAll() as $lista):
-    echo "<br> Nome: ".$lista['nome'];
-endforeach;
 
 
 ?>
