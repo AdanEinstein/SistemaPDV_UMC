e36849e9cedcd632cf6505bedfdfa914d07fb9af
Caio Estevan Fernandes <11211100566@alunos.umc.br>
2022-05-04 13:20:15 -0300

    confirmar em master

--- actions/actionAtualizarUser.php
+++ actions/actionAtualizarUser.php
@@ -2,12 +2,12 @@
 session_start();
 require_once '../banco/conexBanco.php';
 $email = $_POST['email'];
-$newPassword = $_POST['password'];
+$password = $_POST['password'];
 $confirmPassword = $_POST['confirmPassword'];
 
 if($password == $confirmPassword)
 {
-    $sql = "UPDATE pdv_usuarios SET :password WHERE :id =)";
+    $sql = "UPDATE pdv_usuarios SET :password WHERE :id)";
     $params = [":password"=>$password, ":perfil"=>'padrão'];
     $conexao = new DAO();
     if ($conexao->executeSQL($sql, $params)) {
--- recuperarSenha.php
+++ recuperarSenha.php
@@ -1,8 +1,27 @@
+<?php
+include_once "includes/header.php";
+session_start();
+?>
+
 <form action="./actions/actionAtualizarUser.php" method="post">
 <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="user" name="user">
-            <label for="user">e-mail:</label>
+            <input type="email" class="form-control" id="email" name="email">
+            <label for="email">Enter your email:</label>
+            
         </div>
         <button type="submit">Enviar</button>
+        <?php
+        if (isset($_SESSION["resposta"])) {
+            $resposta = $_SESSION["resposta"];
+            echo '<br><div class="alert alert-primary" role="alert">'.
+                $resposta
+                .'</div><br>';
+            unset($_SESSION["resposta"]);
+        }
+        ?>
+        
 </form>
 
+<?php
+include_once "includes/footer.php"
+?>
